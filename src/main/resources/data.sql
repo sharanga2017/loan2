7ff733a4f14701a96fb645d89d19e89d32e36ebd
Serguei Khan <33214783+sharanga2017@users.noreply.github.com>
2021-06-02 00:34:07 +0200

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
-INSERT INTO table (amount, participation, duration, interestRate,insuranceRate,
-jobLossInsurance, couverture, dateRequest)
- VALUES (1888, 233, 12, 3.67, 12.6 ,true, 60 , 2020-10-10);
+INSERT INTO demands (amount, participation, duration, interest_rate,insurance_rate,
+job_lost_insurance, couverture, date_request)
+ VALUES (700000, 300000, 12, 23.46, 12.09 ,1, 60 , '2021-06-02 00:17:41');
 
-
+INSERT INTO demands (amount, participation, duration, interest_rate,insurance_rate,
+job_lost_insurance, couverture, date_request)
+ VALUES (800000, 450000, 12, 15.46, 12.09 ,1, 60 , '2021-06-02 00:17:41');
+INSERT INTO demands (amount, participation, duration, interest_rate,insurance_rate,
+job_lost_insurance, couverture, date_request)
+ VALUES (900000, 30000, 12, 44.46, 12.09 ,1, 60 , '2021-06-02 00:17:41');
+ INSERT INTO demands (amount, participation, duration, interest_rate,insurance_rate,
+job_lost_insurance, couverture, date_request)
+ VALUES (400000, 600000, 12, 23.46, 12.09 ,1, 60 , '2021-06-02 00:17:41');
